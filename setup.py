6079d8cdf49afefe5caa9521fe3cd5136fc774d0
Kostya Rybnikov <k-bx@k-bx.com>
2012-05-09 18:54:51 +0300

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name='mockstar',
-      version='0.1.2',
+      version='0.1.3',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       author='Konstantine Rybnikov',
