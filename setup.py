51551ca5717cd7cf9d06c46a8773917746a31915
Kostya Rybnikov <k-bx@k-bx.com>
2012-05-09 17:49:00 +0300

    add long description

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 from distutils.core import setup
 
 DESCRIPTION = 'Small mocking/unit-testing improvements on top of mock library'
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(name='mockstar',
       version='0.1',
       description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
       author='Konstantine Rybnikov',
       author_email='k-bx@k-bx.com',
       url='https://bitbucket.org/k_bx/mockstar',
